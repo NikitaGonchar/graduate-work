e9dbb8adb2f43050f48f8626e5c6b814dd1a6dba
Gonchar Nikita <nikitagonchar33@gmail.com>
2023-02-07 10:31:21 +0300

    new commit dyplom

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,10 +13,7 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('main') }}">VapeBaraholka</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        
         <form action="{{ route('main') }}">
             <div class="input-group">
                 <input class="form-control" type="text" placeholder="Название" name="title"
